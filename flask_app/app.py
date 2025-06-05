50c2d5de108b5d79940e3db16e930d6e2feeee48
Alex Torrance <hello@alextorrance.co.uk>
2025-06-05 23:26:13 +0100

    Create a webpage in the Flask app to show a random place name

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template_string
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from generate_placename import generate_common_placenames
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    placename = generate_common_placenames()
+    html = """
+    <html>
+        <head><title>Random Placename Generator</title></head>
+        <body>
+            <h1>Random Placename</h1>
+            <p style="font-size:2em;">{{ placename }}</p>
+            <form method="get">
+                <button type="submit">Generate Another</button>
+            </form>
+        </body>
+    </html>
+    """
+    return render_template_string(html, placename=placename)
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+if __name__ == "__main__":
+    app.run(debug=True)
