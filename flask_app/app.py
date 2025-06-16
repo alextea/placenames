65fae223af5a20c2ddb2eb4be5298f7e69aa2ece
Alex Torrance <hello@alextorrance.co.uk>
2025-06-16 23:24:55 +0100

    Style the page to look like a road sign

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 from placenames.generate_placename import generate_common_placenames
 
 app = Flask(__name__)
@@ -7,20 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     placename = generate_common_placenames()
-    html = """
-    <html>
-        <head><title>Random Placename Generator</title></head>
-        <body>
-            <h1>Random Placename</h1>
-            <p style="font-size:2em;">{{ placename }}</p>
-            <form method="get">
-                <button type="submit">Generate Another</button>
-            </form>
-        </body>
-    </html>
-    """
-    return render_template_string(html, placename=placename)
-
+    return render_template("home.html", placename=placename)
 
 if __name__ == "__main__":
     app.run(debug=True)
